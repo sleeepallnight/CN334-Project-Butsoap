7a88dd7097f002821ff79d876afe4031ac44a205
chatcharin <chatcharin2004@gmail.com>
2025-05-18 23:53:27 +0700

    fix render

--- product_service/settings.py
+++ product_service/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": os.environ.get("5432"),
+        "PORT": "5432",
     }
 }
 
